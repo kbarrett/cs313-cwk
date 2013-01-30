f48983aec852dd74250ec80ca508c340147e86c4
kbarrett <x_kimmi_13_x@hotmail.co.uk>
2013-01-30 19:31:17 +0000

    Initial commit

---
+++ src/robot/TestClass.java
@@ -0,0 +1,12 @@
+package robot;
+import lejos.nxt.*;
+
+public class TestClass
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Hello World.");
+		Button.waitForAnyPress();
+	}
+	
+}
